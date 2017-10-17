1f3c067d7d01fcba8e36d3d39494e24cb59dbd56
kevileem2 <32266918+kevileem2@users.noreply.github.com>
2017-10-17 09:59:28 +0200

    Added files for assignement 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $naam = "Kevin";
+    echo "<h1>Welkom $naam</h1>";
+    ?>
+</body>
+</html>
